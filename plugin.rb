c70825a07055fdb3718a45763a661457b937f468
Daniel Stadelman <bioradmeister@gmail.com>
2015-10-05 13:47:03 -0600

    added inital code for monkey patch

---
+++ plugin.rb
@@ -0,0 +1,11 @@
+# name: user-custom-field-trigger
+# about: Adds a DiscourseEvent trigger to the UserCustomField on after_save
+# version: 1
+# authors: Daniel Stadelman
+# url: https://github.com/ruffbytes/discourse-user-custom-field-trigger
+
+class UserCustomField
+  after_save do
+    DiscourseEvent.trigger(:user_custom_field_changed, self.user_id, self.name, self.value)
+  end
+end
